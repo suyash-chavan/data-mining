3bf6a51123a46c6fda9ef9b15ba84f701a992931
Suyash Chavan <suyashc222@gmail.com>
2022-09-16 14:53:34 +0530

    Initial

--- app/app.py
+++ app/app.py
@@ -452,21 +452,24 @@ def tree_to_code(tree, feature_names):
         feature_names[i] if i != _tree.TREE_UNDEFINED else "undefined!"
         for i in tree_.feature
     ]
-    print("def tree({}):".format(", ".join(feature_names)))
+
+    rules = ("def tree({}):".format(", ".join(feature_names)))
 
     def recurse(node, depth):
         indent = "  " * depth
         if tree_.feature[node] != _tree.TREE_UNDEFINED:
             name = feature_name[node]
             threshold = tree_.threshold[node]
-            print("{}if {} <= {}:".format(indent, name, threshold))
+            rules = rules + "\n" + ("{}if {} <= {}:".format(indent, name, threshold))
             recurse(tree_.children_left[node], depth + 1)
-            print("{}else:  # if {} > {}".format(indent, name, threshold))
+            rules = rules + "\n" + ("{}else:  # if {} > {}".format(indent, name, threshold))
             recurse(tree_.children_right[node], depth + 1)
         else:
-            print("{}return {}".format(indent, tree_.value[node]))
+            rules = rules + "\n" + ("{}return {}".format(indent, tree_.value[node]))
 
     recurse(0, 1)
+
+    return rules
 
 def decisiontree(dataframe):
     columns = list(dataframe.columns)
